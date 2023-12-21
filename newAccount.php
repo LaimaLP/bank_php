1462e02f3aceade90c7f9297e2703757a33d38a3
LaimaLP <pileliene.laima@gmail.com>
2023-12-21 22:19:54 +0200

    created pages

---
+++ newAccount.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>New Account</title>
+</head>
+<body>
+    <h1> Headeris: meniu su nuorodomis </h1>
+    <h1>New Account </h1>
+    <form action="" method="post">
+        <input type="text" name="name" placeholder="name">
+        <input type="text" name="lastname" placeholder="lastname">
+        <input type="text" name="IBAN" placeholder="LT01 1111 1111 1111 1111">
+        <input type="text" name="PC" placeholder="11111111111">
+        <button type="submit">Create account</button>
+    </form>
+</body>
+</html>
+
+
+
+
+
+
+<!-- Naujos sąskaitos sukūrimas (įvedami duomenys: vardas, pavardė, sąskaitos numeris, asmens kodas) -->
---
+++ nuskaiciuotiLesas.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add money </title>
+</head>
+<body>
+    <h1> Headeris: meniu su nuorodomis </h1>
+    <h1>Nuskaiciuoti lesas </h1>
+    <p> Name: ... </p>
+    <p> Last Name: ... </p>
+    <p> Saskaitos Likutis: ... </p>
+    <form action="" method="post">
+        <input type="text" name="removeMoney" placeholder="€1111">
+        <button type="submit">Remove money</button>
+    </form>
+</body>
+</html>
